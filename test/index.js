f660cfc06c1929deadf03439b54af0863607fcb2
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-01-10 12:44:14 +0530

    Remove commented test

--- test/index.js
+++ test/index.js
@@ -84,30 +84,6 @@ describe('Initialize wallet ', () => {
         console.log("acc ", acc)
     })
 
-    // it("Get fees", async () => {
-    //     const accounts = await avalancheKeyring.getAccounts()
-    //     const web3 = new Web3(TESTNET.URL);
-
-    //     const tokenContractAvalanche = new web3.eth.Contract(
-    //         tokenContract.abi,
-    //         tokenContract.networks[`${TESTNET.CHAIN_ID}`].address
-    //     );
-
-    //     const txData = tokenContractAvalanche.methods.mint(CONTRACT_MINT_PARAM.amount);
-    //     const data = txData.encodeABI();
-
-    //     const tx = {
-    //         from: accounts[0],
-    //         to: AVALANCHE_CONTRACT,
-    //         value: AVALANCHE_AMOUNT_TO_CONTRACT,
-    //         data
-    //     }
-
-    //     const fees = await avalancheKeyring.getFees(tx, web3)
-    //     console.log("fees ", fees)
-
-    // })
-
     it("Get fees with manual gasLimit", async () => {
         const web3 = new Web3(TESTNET.URL);
         const tx = {
